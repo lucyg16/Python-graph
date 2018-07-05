0a9f6552760f51df79d61e583f4e14fc9f6111bf
Lucy <lucyguo@doe6966688.dhcp.lbnl.us>
2018-07-05 16:32:46 -0700

    add to idea

--- idea.py
+++ idea.py
@@ -1,8 +1,8 @@
 # ask user for input file of data
 input = "inputFile"
 
-# ask user for desired index
-index_d = x
+# ask user for desired index d
+index = d
 
 # create dictionary to add values to
 list_of_index_d = []
@@ -12,8 +12,14 @@ for i in input_row;
     for j in input_column:
         if j[d] not in dictionary
             # create list to append to dictionary of that value
-            # add j[d] to list_of_index_d if j[d] does not already exist
+            # add j[d] to list_of_index_d if j[d] as identifier if does not already exist
+            # then add the corresponding value over in the other matrix
         else
             #add the value to the list with identifier j[d] in the dictionary
 
+
+
+# now you should have the dictionary of identifiers with lists of values
+
+#now make a box plot using the code provieded on matplotlib, replacing the input with the dictionary that you have created
 
