c1ec37ed89744f259629d238cd83e0382094b9a7
Lucy <lucyguo@doe6966688.dhcp.lbnl.us>
2018-07-05 16:23:20 -0700

    idea

---
+++ idea.py
@@ -0,0 +1,19 @@
+# ask user for input file of data
+input = "inputFile"
+
+# ask user for desired index
+index_d = x
+
+# create dictionary to add values to
+list_of_index_d = []
+
+#iterate through list of given data
+for i in input_row;
+    for j in input_column:
+        if j[d] not in dictionary
+            # create list to append to dictionary of that value
+            # add j[d] to list_of_index_d if j[d] does not already exist
+        else
+            #add the value to the list with identifier j[d] in the dictionary
+
+
